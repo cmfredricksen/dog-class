2f84f38922e96a2f1970c86cdf15248f4d29c150
Christine Fredricksen <cmfredricksen@gmail.com>
2022-03-22 13:55:11 -0500

    added icons, added icon to header, made header links

--- src/lib/header.svelte
+++ src/lib/header.svelte
@@ -1,9 +1,15 @@
 <script>
 	import Nav from './nav.svelte';
+	import '../../static/global.css';
 </script>
 
 <header>
-	<h1>Dog Class</h1>
+	<a href="/">
+		<h1>Dog Class</h1>
+	</a>
+	<a href="/">
+		<i class="material-icons-outlined"> pets </i>
+	</a>
 	<Nav />
 </header>
 
@@ -11,10 +17,26 @@
 	header {
 		display: flex;
 		justify-content: space-between;
-		align-items: baseline;
+		align-items: center;
 		padding: 1rem 1rem 2rem;
 		color: var(--clr-pale-green);
 		border-bottom: 2px solid var(--clr-pale-green);
 		outline: 1px solid var(--clr-white);
+	}
+
+	header i {
+		border: 2px solid var(--clr-pale-green);
+		padding: 0.35rem;
+		border-radius: 1.5rem;
+		color: var(--clr-magenta);
+	}
+
+	header i:hover {
+		/* border: 2px solid var(--clr-white); */
+		color: var(--clr-white);
+	}
+
+	header h1:hover {
+		color: var(--clr-white);
 	}
 </style>
